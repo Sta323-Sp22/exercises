694630ba64c210e2901d9584e18a4281dd7f2b4c
Colin Rundel <rundel@gmail.com>
2022-03-30 13:15:19 -0400

    class

--- class_2022_03_30.R
+++ class_2022_03_30.R
@@ -5,3 +5,10 @@ nc = read_sf("data/gis/nc_counties/", quiet=TRUE)
 air = read_sf("data/gis/airports/", quiet=TRUE)
 hwy = read_sf("data/gis/us_interstates/", quiet=TRUE)
 
+nc %>% 
+  select(-AREA:FIPS) %>%
+  mutate(
+    intersect = st_intersects(nc, air),
+    n_air = map_int(intersect, length)    
+  ) %>%
+  filter(n_air > 0)
